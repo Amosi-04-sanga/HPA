489f8d6a82270f68c3fddac28113e4a21f326137
amosi sanga <sangaamosi04@gmail.com>
2024-11-09 20:55:17 +0300

    text in hero section changed

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         
             <Fade>
               <p className="mt-4 text-white font-[900] text-[40px]">
-              HEALTH PROMOTION <br/> ALLIANCE amo
+              HEALTH PROMOTION <br/> ALLIANCE
               </p>
             </Fade>
           </div>
