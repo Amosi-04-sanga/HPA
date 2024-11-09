c3629de9834350fa8097b472f406d9e9722f910c
amosi sanga <sangaamosi04@gmail.com>
2024-11-09 21:24:51 +0300

    text in hero section changed

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -43,7 +43,7 @@ const NavBar = () => {
     <div className='sticky top-0 left-0 z-10 flex justify-between items-center text-[13px] p-2 h-[75px] bg-[#D21034]'>
         <div className='ml-4 flex flex-col items-center'>
         <p className="text-white mb-1 no-underline">
-            info@nhptz.org
+            info@hpatz.org
         </p>
         <div className="flex gap-4 text-white">
                   <Image src="/assets/icons/facebook.svg" alt='facebook' width={20} height={20}/>
